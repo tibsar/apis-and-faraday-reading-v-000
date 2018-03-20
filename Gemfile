312c0c1247215f6fe4401c4da2ead1c54b8fa18e
Sara Tibbetts <sxt4866@rit.edu>
2018-03-20 00:14:27 +0000

    automatically backed up by learn

--- Gemfile
+++ Gemfile
@@ -45,3 +45,7 @@ group :development do
   gem 'spring'
 end
 
+gem 'faraday'
+
+gem 'pry'
+
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,5 +3,13 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
+    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+      req.params['client_id'] = "EDGKYN2I4DDTXW4SVFUZS3SYW3NGQJWQTEPQ2FE2HPTJDKOV"
+      req.params['client_secret'] = "UWOPGCE4OIHE4XSSI5JKFXSH0XJRKQQLVOIDPTSH4VP1HCOV"
+      req.params['v'] = '20160201'
+      req.params['near'] = params[:zipcode]
+      req.params['query'] = 'coffee shop'
+    end
+    render 'search'
   end
 end
