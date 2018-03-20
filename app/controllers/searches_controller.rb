607e635aedc69f7daa8ebb8676f2e3d1a647b862
Sara Tibbetts <sxt4866@rit.edu>
2018-03-20 00:15:12 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,6 +10,8 @@ class SearchesController < ApplicationController
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
     end
+    
+    
     render 'search'
   end
 end
